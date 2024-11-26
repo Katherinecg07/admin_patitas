3859b00975497c5edab56fb223d3b618f2f97811
Katherinecg07 <katherine.carvajal.g@gmail.com>
2024-11-26 19:22:55 -0300

    creación del modelo Provider

--- administration/models.py
+++ administration/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Provider(models.Model):
+    first_name = models.CharField(max_length=128) # caracteres
+    last_name = models.CharField(max_length=128)
+    main_photo = models.ImageField(upload_to="providers/", 
+                                   blank=True, null=True)
+    description = models.TextField()
+    
